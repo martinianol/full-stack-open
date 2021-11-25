9a7ddb713b52fb3deecfb472ccaa8be457460a57
Mars <martinianol@outlook.com>
2021-11-25 11:57:49 -0300

    1.7: unicafe step2

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -4,26 +4,52 @@ const Header = () => <h1>give feedback</h1>;
 
 const Button = ({ onClick, text }) => (<button onClick={onClick}>{text}</button>);
 
-const Statistics = ({ good, neutral, bad }) => {
+const Statistics = ({ statistics }) => {
   return (
     <>
       <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
+      <p>good {statistics.good}</p>
+      <p>neutral {statistics.neutral}</p>
+      <p>bad {statistics.bad}</p>
+      <p>total {statistics.total}</p>
+
     </>
   )
 }
 
 const App = () => {
   // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [total, setTotal] = useState(0);
 
-  const increaseGood = () => setGood(good + 1);
-  const increaseNeutral = () => setNeutral(neutral + 1);
-  const increaseBad = () => setBad(bad + 1);
+
+
+
+  const statistics = {
+    good: good,
+    neutral: neutral,
+    bad: bad,
+    total: total,
+  }
+
+  const increaseGood = () => {
+    setGood(good + 1);
+    setTotal(total + 1);
+  };
+  const increaseNeutral = () => {
+    setNeutral(neutral + 1);
+    setTotal(total + 1);
+  };
+  const increaseBad = () => {
+    setBad(bad + 1);
+    setTotal(total + 1);
+  };
+
+  const average = (good - bad) / (total);
+  const positive = good / total * 100;
+
 
 
   return (
@@ -32,9 +58,13 @@ const App = () => {
       <Button onClick={increaseGood} text='good' />
       <Button onClick={increaseNeutral} text='neutral' />
       <Button onClick={increaseBad} text='bad' />
-      <Statistics good={good} neutral={neutral} bad={bad} />
+      <Statistics statistics={statistics} />
+      <p>average {isNaN(average) ? '-' : average}</p>
+      <p>positive {isNaN(positive) ? '-' : `${positive} %`} </p>
     </div>
   )
+
+
 }
 
 export default App
