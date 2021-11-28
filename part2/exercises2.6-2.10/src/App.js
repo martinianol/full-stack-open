f4f9e6a825ebd9e1b18f1bac86fcbdaca8b9a7e5
Mars <martinianol@outlook.com>
2021-11-28 17:54:52 -0300

    2.9*: The Phonebook Step4

--- part2/exercises2.6-2.10/src/App.js
+++ part2/exercises2.6-2.10/src/App.js
@@ -1,15 +1,17 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    {
-      name: 'Arto Hellas',
-      phone: 40841000,
-      id: 1
-    }
-  ])
+  const [persons, setPersons] = useState(
+    [
+      { name: 'Arto Hellas', number: '040-123456', id: 1 },
+      { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+      { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+      { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+    ]
+  )
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [nameFiltered, setNameFiltered] = useState('');
 
   const addNumber = (event) => {
     event.preventDefault();
@@ -20,12 +22,13 @@ const App = () => {
 
     const personObject = {
       name: newName,
-      phone: newNumber,
+      number: newNumber,
       id: persons.length + 1
     }
 
     setPersons(persons.concat(personObject))
     setNewName('')
+    setNewNumber('')
   }
 
   const handlePersonChange = (event) => {
@@ -36,13 +39,21 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFiltered = (event) => {
+    setNameFiltered(event.target.value)
+  }
+
 
 
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={nameFiltered} onChange={handleFiltered} />
+      </div>
       <form onSubmit={addNumber}>
+        <h2>add a new</h2>
         <div>
           name: <input value={newName} onChange={handlePersonChange} />
         </div>
@@ -54,8 +65,17 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person =>
-          <li key={person.id}>{person.name} {person.phone}</li>
+        {persons.map(person => {
+
+          if (person.name.toLowerCase().includes(nameFiltered.toLowerCase())) {
+            return <li key={person.id}>{person.name} {person.number}</li>
+          } else {
+            return null
+          }
+
+
+
+        }
         )}
       </ul>
     </div>
