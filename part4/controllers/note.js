725ec6076058b27782e045a4a658284af83e9297
Mars <martinianol@outlook.com>
2021-12-16 06:57:43 -0300

    Project Structure

---
+++ part4/controllers/note.js
@@ -0,0 +1,77 @@
+const notesRouter = require('express').Router()
+const Note = require('../models/note')
+
+/* notesRouter.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+}) */
+
+notesRouter.get('/', (request, response) => {
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
+})
+
+notesRouter.get('/:id', (request, response, next) => {
+  const id = request.params.id
+  Note.findById(id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
+notesRouter.delete(':id', (request, response, next) => {
+  const id = request.params.id
+  Note.findByIdAndRemove(id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+notesRouter.post('/', (request, response, next) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+  })
+
+  note.save()
+    .then(savedNote => savedNote.toJSON())
+    .then(savedAndFormattedNote => {
+      response.json(savedAndFormattedNote)
+    })
+    .catch(error => next(error))
+
+})
+
+notesRouter.put('/:id', (request, response, next) => {
+  const body = request.body
+  const id = request.params.id
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
+
+
+module.exports = notesRouter
