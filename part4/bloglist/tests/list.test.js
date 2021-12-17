2cbfdb10ed7fc19940d3b91d4698ab1d96f9073c
Mars <martinianol@outlook.com>
2021-12-17 16:54:21 -0300

    4.4: helper functions and unit tests, step2

--- part4/bloglist/tests/list.test.js
+++ part4/bloglist/tests/list.test.js
@@ -6,3 +6,27 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
 })
+
+describe('total likes', () => {
+  test('of empty list is zero', () => {
+    const blogs = []
+    const result = listHelper.totalLikes(blogs)
+    expect(result).toBe(0)
+  })
+  test('when list has only one blog equals the likes of that', () => {
+    const blogs = [{
+      title: "Test Blog",
+      author: "Test Author",
+      url: "www.test.com",
+      likes: 20
+    }]
+    const result = listHelper.totalLikes(blogs)
+    expect(result).toBe(blogs[0].likes)
+  })
+  test('of a larger list is calculated right', () => {
+    const blogs = [{ likes: 20 }, { likes: 20 }, { likes: 20 }
+    ]
+    const result = listHelper.totalLikes(blogs)
+    expect(result).toBe(60)
+  })
+})
