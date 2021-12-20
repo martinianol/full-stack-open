249e93fc9ef3000afbe9f74172aef77e80dfb215
Mars <martinianol@outlook.com>
2021-12-20 11:41:35 -0300

    4.10: Blog list tests, step3

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -48,3 +48,33 @@ test('there is an id property', async () => {
 
   expect(_.has(firstBlog, 'id')).toBeTruthy()
 })
+
+test('it creates a new blog', async () => {
+
+  const newBlog = {
+    title: "Blog Test",
+    author: "Test author",
+    url: "www.test.com",
+    likes: 10,
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+
+  const blogsAtEnd = await helper.blogsInDb()
+
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+
+  const titles = blogsAtEnd.map(b => b.title)
+
+  expect(titles).toContain('Blog Test')
+
+
+
+
+
+})
