920c7ef36849465cc011e8242ee3fe6c315169ce
Mars <martinianol@outlook.com>
2021-12-20 18:24:58 -0300

    Problems of Token-based authentication

--- part4/lesson/controllers/note.js
+++ part4/lesson/controllers/note.js
@@ -1,6 +1,8 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
 
 notesRouter.get('/', async (request, response) => {
   const notes = await Note.find({}).populate('user', { username: 1, name: 1 })
@@ -25,10 +27,25 @@ notesRouter.delete('/:id', async (request, response, next) => {
   response.status(204).end()
 })
 
+//Authentication
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
+
 notesRouter.post('/', async (request, response, next) => {
   const body = request.body
 
-  const user = await User.findById(body.userId)
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
 
   if (!body.content) {
     return response.status(400).json({
