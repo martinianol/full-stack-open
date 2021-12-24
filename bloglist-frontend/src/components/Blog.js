048259c63f92134a95588e177eb7c96cfbef5822
Mars <martinianol@outlook.com>
2021-12-24 17:33:58 -0300

    5.7* Blog list frontend, step7

--- bloglist-frontend/src/components/Blog.js
+++ bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,43 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from 'react'
+const Blog = ({ blog }) => {
+  const [view, setView] = useState(false)
+  const [viewHide, setViewHide] = useState('view')
+
+  const handleView = () => {
+    setView(!view)
+    viewHide === 'view' ? setViewHide('hide') : setViewHide('view')
+  }
+
+  const details = () => {
+    return (
+      <div>
+        <p>
+          url: {blog.url}
+        </p>
+        <p>
+          likes: {blog.likes} <button>like</button>
+        </p>
+        <p>
+          user: {blog.user.username}
+        </p>
+      </div>
+    )
+  }
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}
+      <button onClick={handleView}>{viewHide} details</button>
+      {view && details()}
+    </div>
+  )
+}
 
 export default Blog
