aa5d5bc6570a65c84e0095d907ff16ff4dee6546
Mars <martinianol@outlook.com>
2021-12-25 09:10:40 -0300

    PropTypes & ESLint

--- part5-notes/src/components/LoginForm.js
+++ part5-notes/src/components/LoginForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import PropTypes from 'prop-types'
 
 const LoginForm = (props) => {
 
@@ -39,6 +40,10 @@ const LoginForm = (props) => {
     </div>
 
   )
+}
+
+LoginForm.propTypes = {
+  handleLogin: PropTypes.func.isRequired
 }
 
 export default LoginForm
