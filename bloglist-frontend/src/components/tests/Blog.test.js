7304b9c1e75bf3174abbbbfa3fae8db32fa4ad1b
Mars <martinianol@outlook.com>
2021-12-26 08:03:49 -0300

    5.15*: Blog list tests, step3

--- bloglist-frontend/src/components/tests/Blog.test.js
+++ bloglist-frontend/src/components/tests/Blog.test.js
@@ -1,20 +1,22 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render, fireEvent, prettyDOM } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from '../Blog'
+import { prettyDOM } from '@testing-library/dom'
+
+const blog = {
+  url: 'www.test.com',
+  likes: 10,
+  user: {
+    username: 'userNameTest',
+    user: 'userTest',
+    id: 1
+  },
+  title: 'Test title',
+  author: 'Test Author'
+}
 
 test('renders content', () => {
-  const blog = {
-    url: 'www.test.com',
-    likes: 10,
-    user: {
-      username: 'userNameTest',
-      user: 'userTest',
-      id: 1
-    },
-    title: 'Test title',
-    author: 'Test Author'
-  }
 
   const component = render(
     <Blog blog={blog} />
@@ -27,3 +29,26 @@ test('renders content', () => {
   expect(component.container).toHaveTextContent(`${blog.title} ${blog.author}`)
 
 })
+
+  / test('clicking the like button twice calls event handler passed as props twice', () => {
+
+    const user = {
+      blogs: []
+    }
+
+    const mockHandler = jest.fn()
+
+    const component = render(
+      <Blog blog={blog} user={user} onUpdate={mockHandler} />
+    )
+    const buttonDetails = component.getByText('view details')
+    fireEvent.click(buttonDetails)
+
+    const buttonLike = component.getByText('like')
+
+    fireEvent.click(buttonLike)
+    fireEvent.click(buttonLike)
+
+    expect(mockHandler.mock.calls).toHaveLength(2)
+
+  })
