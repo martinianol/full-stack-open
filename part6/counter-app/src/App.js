7dc4894ee15e8d0ec69c2ea48d0bb73ed95bc8ef
Mars <martinianol@outlook.com>
2022-01-01 21:15:34 -0300

    Redux DevTools2

--- part6/counter-app/src/App.js
+++ part6/counter-app/src/App.js
@@ -5,9 +5,6 @@ import VisibilityFilter from './components/VisibilityFilter'
 
 
 const App = () => {
-  const filterSelected = (value) => {
-    console.log(value)
-  }
   return (
     <div>
       <NewNote />
