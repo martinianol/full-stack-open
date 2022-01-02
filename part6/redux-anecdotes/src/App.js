b29aacf95f7af65dae484a448656caa1e9b0d3c0
Mars <martinianol@outlook.com>
2022-01-02 18:51:26 -0300

    6.13 Anecdotes and the backend, step1

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Filter from './components/Filter'
+import { useDispatch } from 'react-redux'
+import anecdoteService from './services/anecdote'
+import { initializeAnecdotes } from './reducers/anecdoteReducer'
 
 const App = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    anecdoteService
+      .getAll().then(anecdotes => dispatch(initializeAnecdotes(anecdotes)))
+  }, [dispatch])
+
   return (
     <div>
       <h2>Anecdotes</h2>
---
+++ part6/redux-anecdotes/src/services/anecdote.js
@@ -0,0 +1,29 @@
+import axios from 'axios'
+
+const baseUrl = 'http://localhost:3001/anecdotes'
+
+const getAll = async () => {
+  const response = await axios.get(baseUrl)
+  return response.data
+}
+
+const createNew = async (content) => {
+  const object = { content, votes: 0 }
+  const response = await axios.post(baseUrl, object)
+  return response.data
+}
+
+const vote = async (id) => {
+  const response = await axios.get(`${baseUrl}/${id}`)
+  const anecdote = response.data
+  const anecdoteChanged = { ...anecdote, votes: anecdote.votes + 1 }
+
+  const responseUpdate = await axios.put(`${baseUrl}/${id}`, anecdoteChanged)
+  return responseUpdate
+}
+
+export default {
+  getAll,
+  createNew,
+  vote
+}
