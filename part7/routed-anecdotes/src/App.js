a9239c3a8da3a2fee6a84635af3c889cd91c2a14
Mars <martinianol@outlook.com>
2022-01-04 11:38:10 -0300

    7.5: anecdotes and hooks step2

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -79,6 +79,13 @@ const CreateNew = (props) => {
     history.push('/')
   }
 
+  const handleReset = (e) => {
+    e.preventDefault()
+    content.reset()
+    author.reset()
+    info.reset()
+  }
+
   return (
     <div>
       <h2>create a new anecdote</h2>
@@ -96,6 +103,7 @@ const CreateNew = (props) => {
           <input name='info' type={info.type} value={info.value} onChange={info.onChange} />
         </div>
         <button>create</button>
+        <button onClick={handleReset}>reset</button>
       </form>
     </div>
   )
