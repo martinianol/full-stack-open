3c175177db1d0cd0ded6b3b51e2c84d3c32cdb67
Mars <martinianol@outlook.com>
2022-01-06 08:29:28 -0300

    Added notification reducer

---
+++ bloglist-frontend/src/reducers/notificationReducer.js
@@ -0,0 +1,39 @@
+const notificationReducer = (state = null, action) => {
+	switch (action.type) {
+		case 'SET_NOTIFICATION': {
+			return action.notification
+		}
+		case 'CLEAR_NOTIFICATION': {
+			return null
+		}
+		default:
+			return state
+	}
+}
+
+let timeoutId
+
+export const setNotification = (notification, time) => {
+	return async dispatch => {
+		dispatch({
+			type: 'SET_NOTIFICATION',
+			notification
+		})
+		if (timeoutId) {
+			clearTimeout(timeoutId)
+		}
+		timeoutId = setTimeout(() => {
+			dispatch({
+				type: 'CLEAR_NOTIFICATION'
+			})
+		}, time * 1000)
+	}
+}
+
+export const clearNotification = (id) => {
+	return {
+		type: 'CLEAR_NOTIFICATION'
+	}
+}
+
+export default notificationReducer
