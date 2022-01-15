407bb8250ac2b8b53d01be894c63391da1f3689d
Mars <martinianol@outlook.com>
2022-01-15 11:05:57 -0300

    exercise 8.2

--- part8/library-exercises-8.1-8.7/index.js
+++ part8/library-exercises-8.1-8.7/index.js
@@ -85,16 +85,29 @@ let books = [
 ]
 
 const typeDefs = gql`
+
+  type Book {
+    title: String!
+    published: Int
+    author: String
+    id: ID!
+    genres: [String]
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Book]
   }
 `
 
 const resolvers = {
   Query: {
     bookCount: () => books.length,
-    authorCount: () => authors.length
+    authorCount: () => authors.length,
+    allBooks: (root, args) => {
+      return books
+    }
   }
 }
 
