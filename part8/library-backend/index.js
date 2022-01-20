a8643376ddf4350e8c8fb3aae63eb20854bf00c9
Mars <martinianol@outlook.com>
2022-01-20 14:05:40 -0300

    8.13: Database, part 1

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -1,5 +1,21 @@
 const { ApolloServer, gql } = require('apollo-server')
 const { v1: uuid } = require('uuid')
+const mongoose = require('mongoose')
+
+const Author = require('./models/author')
+const Book = require('./models/book')
+
+const MONGODB_URI = 'mongodb+srv://fullstack:20303574@cluster0.yrs0y.mongodb.net/library-app?retryWrites=true' //check this line if it doesnt work
+
+console.log('connecting to', MONGODB_URI)
+
+mongoose.connect(MONGODB_URI)
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connection to MongoDB:', error.message)
+  })
 
 let authors = [
   {
@@ -26,12 +42,6 @@ let authors = [
     id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
   },
 ]
-
-/*
- * English:
- * It might make more sense to associate a book with its author by storing the author's id in the context of the book instead of the author's name
- * However, for simplicity, we will store the author's name in connection with the book
-*/
 
 let books = [
   {
@@ -89,10 +99,10 @@ const typeDefs = gql`
 
   type Book {
     title: String!
-    published: Int
-    author: String!
+    published: Int!
+    author: Author!
+    genres: [String!]!
     id: ID!
-    genres: [String]
   }
 
   type Author {
@@ -106,7 +116,7 @@ const typeDefs = gql`
     bookCount: Int!
     authorCount: Int!
     allBooks(author: String, genre: String): [Book]
-    allAuthors: [Author]
+    allAuthors: [Author!]!
   }
 
   type Mutation {
@@ -127,9 +137,12 @@ const typeDefs = gql`
 
 const resolvers = {
   Query: {
-    bookCount: () => books.length,
-    authorCount: () => authors.length,
-    allBooks: (root, args) => {
+    bookCount: () => Book.collection.countDocuments(),
+    authorCount: () => Author.collection.countDocuments(),
+    allBooks: async (root, args) => {
+      return await Book.find({})
+
+
       if (!args.author && !args.genre) {
         return books
       } else if (args.author && !args.genre) {
@@ -141,13 +154,15 @@ const resolvers = {
         return booksAuthor.filter(book => book.genres.includes(args.genre))
       }
     },
-    allAuthors: (root, args) => {
-      authors.map(author => {
-        bookCount = books.filter(book => book.author === author.name).length
-        author.bookCount = bookCount
-        return author
-      })
-      return authors
+    allAuthors: async (root, args) => {
+      return await Author.find({})
+
+      /*  authors.map(author => {
+         bookCount = books.filter(book => book.author === author.name).length
+         author.bookCount = bookCount
+         return author
+       })
+       return authors */
     }
   },
   Mutation: {
